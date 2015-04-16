b6022f76c8022ada027799597f1a17754c993fa1
Hoa Nguyen & Kevin Alwell & Awesome Ninja Fighter & Tracy Teague <pair+hoathenguyen85.kevalwell.samguergen.tracyteague@devbootcamp.com>
2015-04-16 19:59:50 -0400

    added 5th character to password. Seed success. Ready to merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 #add user photo url/upload in migration and seed?
 
-sam = {email: "sam@gmail.com", password:"1212", password_confirmation: "1212", first_name: "Samantha", last_name: "Guergenenov", gender: "F", age: 24}
-trace = {email: "tracy@gmail.com", password:"2323", password_confirmation: "2323", first_name: "Tracy", last_name: "Teague", gender: "F", age: 24}
-hoa = {email: "hoa@gmail.com", password:"3434", password_confirmation: "3434", first_name: "Hoa", last_name: "Nguyen", gender: "M", age: 29}
-kev = {email: "kevin@gmail.com", password:"4545", password_confirmation: "4545", first_name: "Kevin", last_name: "Alwell", gender: "M", age: 17}
+sam = {email: "sam@gmail.com", password:"12122", password_confirmation: "12122", first_name: "Samantha", last_name: "Guergenenov", gender: "F", age: 24}
+trace = {email: "tracy@gmail.com", password:"23233", password_confirmation: "23233", first_name: "Tracy", last_name: "Teague", gender: "F", age: 24}
+hoa = {email: "hoa@gmail.com", password:"34344", password_confirmation: "34344", first_name: "Hoa", last_name: "Nguyen", gender: "M", age: 29}
+kev = {email: "kevin@gmail.com", password:"45455", password_confirmation: "45455", first_name: "Kevin", last_name: "Alwell", gender: "M", age: 17}
 
 samsam = User.create(sam)
 tracetrace = User.create(trace)
@@ -25,14 +25,14 @@ prop_4 = FavoriteProperty.create(prop4)
 prop_5 = FavoriteProperty.create(prop5)
 prop_6 = FavoriteProperty.create(prop6)
 
-#type is rating or distance
-crit1 = {description: "museum", type: "distance", api_url: "http://api.yelp.com/v2/search"}
-crit2 = {description: "workplace", type: "distance", api_url: "http://maps.googleapis.com/maps/api/directions/output"}
-crit3 = {description: "park", type: "distance", api_url: "http://api.yelp.com/v2/search"}
-crit4 = {description: "school", type: "rating", api_url: "http://api.greatschools.org/schools/"}
-crit5 = {description: "price", type: "rating", api_url: "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm"}
-crit6 = {description: "crime", type: "rating", api_url: "http://api.spotcrime.com/crimes.json"}
-crit7 = {description: "food", type: "distance", api_url: "http://api.yelp.com/v2/search"}
+#kind is rating or distance
+crit1 = {description: "museum", kind: "distance", api_url: "http://api.yelp.com/v2/search"}
+crit2 = {description: "workplace", kind: "distance", api_url: "http://maps.googleapis.com/maps/api/directions/output"}
+crit3 = {description: "park", kind: "distance", api_url: "http://api.yelp.com/v2/search"}
+crit4 = {description: "school", kind: "rating", api_url: "http://api.greatschools.org/schools/"}
+crit5 = {description: "price", kind: "rating", api_url: "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm"}
+crit6 = {description: "crime", kind: "rating", api_url: "http://api.spotcrime.com/crimes.json"}
+crit7 = {description: "food", kind: "distance", api_url: "http://api.yelp.com/v2/search"}
 
 crit_1 = Criterium.create(crit1)
 crit_2 = Criterium.create(crit2)
