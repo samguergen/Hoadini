64d07fa6e911b27931527bc8c63e6e98eeefa002
Alwell <robinalwell@Robins-MacBook-Pro.local>
2015-04-16 18:41:50 -0400

    properties thumbnail added to user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
 def show
 	#@user = User.find_by(id: session[:user_id])
+	@properties = Favorite_property.where(user_id: session:[user_id])
 	@user = User.create(email: 'kevalwell@gmail.com', avatar: '/https://avatars0.githubusercontent.com/u/8462375?v=3&s=460', password_digest: '123',first_name:'kevin', last_name: 'alwell', gender: 'Male', age: 22 )
 end
 
