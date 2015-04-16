86a8c40647dd5791e83430c7bcfa4bb9eac7501d
Tracy Teague <tracy.teague05@gmail.com>
2015-04-16 19:18:31 -0400

    Add validations and associations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :favorite_properties
+  has_many :user_preferences
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
