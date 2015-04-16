5c44e6e05295467c2c20a2235b92ccb5e7e7fa9a
Hoa Nguyen & Kevin Alwell & Awesome Ninja Fighter & Tracy Teague <pair+hoathenguyen85.kevalwell.samguergen.tracyteague@devbootcamp.com>
2015-04-16 16:43:58 -0400

    Made criterium migration and model 	new file:   app/models/criterium.rb 	new file:   db/migrate/20150416204308_create_criteria.rb 	modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416203904) do
+ActiveRecord::Schema.define(version: 20150416204308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "criteria", force: :cascade do |t|
+    t.string   "description", null: false
+    t.string   "type",        null: false
+    t.string   "api_url",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "favorite_properties", force: :cascade do |t|
     t.string   "address",    null: false
