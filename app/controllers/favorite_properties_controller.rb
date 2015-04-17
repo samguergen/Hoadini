f5de119af147bb5f4ac3b099ab7e876e754ccbe9
kevalwell@gmail.com <robinalwell@Robins-MacBook-Pro.local>
2015-04-17 08:42:38 -0400

    favorite_property page added

--- app/controllers/favorite_properties_controller.rb
+++ app/controllers/favorite_properties_controller.rb
@@ -1,10 +1,10 @@
-class SessionsController < ApplicationController
+class FavoritePropertiesController < ApplicationController
 
 	def create
 		FavoriteProperty.create(params)
 	end
 
 	def show
-		@properties = FavoriteProperty.where(user_id: session[:user_id])
+		@properties = FavoriteProperty.where(user_id: params)
 	end
 end
