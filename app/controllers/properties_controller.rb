7858f3cc9fdefcaec46468b5c288737aa3deedbe
Hoa Nguyen <hoathenguyen85@gmail.com>
2015-04-18 14:45:34 -0500

    Disable turbolink and first try at API call with Httparty 	modified:   .gitignore 	modified:   Gemfile 	modified:   Gemfile.lock 	new file:   app/assets/javascripts/apis/zilyo.js 	modified:   app/assets/javascripts/application.js 	modified:   app/controllers/application_controller.rb 	modified:   app/controllers/properties_controller.rb 	modified:   app/controllers/users_controller.rb 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/properties/show.html.erb 	modified:   config/application.rb 	modified:   config/environment.rb 	modified:   db/schema.rb 	new file:   lib/api.rb

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,6 +5,16 @@ class PropertiesController < ApplicationController
 	end
 
 	def show
-
 	end
+
+  def find_property
+    @z = HTTParty.get('https://zilyo.p.mashape.com/search',
+                      {query: {nelatitude: '22.37',
+                               nelongitude: '-154.48000000000002',
+                               swlatitude: '18.55',
+                               swlongitude: '-160.52999999999997',
+                               isinstantbook: 'true',
+                               provider: 'airbnb,housetrip'},
+                       headers: {'X-Mashape-Key' => 'Aq8RN3VWDnmshWqAaThekfgTPEbap1a3Tn3jsnBYV3fjrNDyQZ'}})
+  end
 end
