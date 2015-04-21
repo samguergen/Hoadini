06fc4a244eec01bf65a420d452c894b8f1b53bf1
Samantha Guergenenov <samguergen@gmail.com>
2015-04-21 18:55:06 -0400

    get ajax for edit appending- but whole page

--- app/assets/javascripts/jaxify.js
+++ app/assets/javascripts/jaxify.js
@@ -38,4 +38,42 @@ $.ajax({
   });
 });
 
+
+//ajax get for displaying edit criteria form
+
+$('#edit').on('click', function(event){
+  event.preventDefault();
+  $.ajax({
+    url: '/user_preferences/#{u.id}/edit'
+  }).done(function(result){
+    $('#edit').toggle(false);
+    $('.preflist').append(result);
+  });
+  });
+
+// ajax edit form
+
+$('.preflist').on('submit', '.newcrit', function(event){
+  event.preventDefault();
+
+  var $form = $( this ),
+
+  crit = $form.find( "input[name='criterium_id']" ).val(),
+  score = $form.find( "input[name='score']" ).val(),
+  search = $form.find( "input[name='search']" ).val();
+  // url = $form.attr( "action" );
+$.ajax({
+    method: 'POST',
+    url: '/user_preferences',
+    data: {
+      criterium_id: crit,
+      score: score,
+      search: search
+    }
+  }).done(function(result){
+    console.log(result);
+    $('.newcrit').prepend(result);
+  });
+});
+
 });
